392952e95edf2c7dfa0cb264d390fda7773313f9
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-28 11:53:39 +0000

    terminado

---
+++ resources/js/components/Main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Main = () => <h1>Hola mundo</h1>;
+
+export default Main;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        // Minificación del js y css basada en la variable de entorno APP_ENV
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // Archivos de entrada
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            // Habilita la recarga en caliente
             refresh: true,
         }),
+        react(),
     ],
 });
