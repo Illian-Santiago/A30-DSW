af2720a6af20ff85e87b81ca91c85bae62f6041f
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-29 03:37:16 +0000

    Update: pagination

--- resources/js/components/ProductList.jsx
+++ resources/js/components/ProductList.jsx
@@ -13,8 +13,8 @@ export default function ProductList() {
         const fetchProducts = async (page) => {
             try {
                 const response = await axios.get(`/api/products?page=${page}`);
-                setProducts(response.data.data); // Asumiendo que los datos están en response.data.data
-                setTotalPages(response.data.total_pages); // Asumiendo que el total de páginas está en response.data.total_pages
+                setProducts(response.data.data);
+                setTotalPages(response.data.meta.last_page);
                 setLoading(false);
             } catch (err) {
                 setError(err.message);
@@ -36,43 +36,35 @@ export default function ProductList() {
         <>
             <div className="container mx-auto p-4">
                 <h1 className="text-3xl font-bold mb-4">Lista de productos</h1>
+
                 <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {products.map(product => (
                         <li key={product.id} className="border rounded-lg p-4 shadow-lg">
                             <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+
                             <p className="text-gray-700 mb-2">{product.description}</p>
                             <p className="text-gray-900 font-bold mb-2">Price: ${product.price}</p>
                             <p className="text-gray-700 mb-2">Stock: {product.stock}</p>
+
                             <img className="w-full h-48 object-cover rounded" src={product.image} alt={product.name} />
                         </li>
                     ))}
                 </ul>
+
                 <div className="flex justify-center mt-4">
-                    <button
-                        className={`px-4 py-2 mx-1 border rounded ${currentPage === 1 ? 'bg-gray-300' : 'bg-white'}`}
-                        onClick={() => handlePageChange(currentPage - 1)}
-                        disabled={currentPage === 1}
-                    >
+                    <button onClick={() => handlePageChange(currentPage - 1)} className={'px-4 py-2 mx-1 border rounded bg-white'} disabled={currentPage === 1} >
                         Previous
                     </button>
-                    {[...Array(totalPages)].map((_, index) => (
-                        <button
-                            key={index}
-                            className={`px-4 py-2 mx-1 border rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-white'}`}
-                            onClick={() => handlePageChange(index + 1)}
-                        >
-                            {index + 1}
-                        </button>
-                    ))}
-                    <button
-                        className={`px-4 py-2 mx-1 border rounded ${currentPage === totalPages ? 'bg-gray-300' : 'bg-white'}`}
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage === totalPages}
-                    >
+
+                    <button className={'px-4 py-2 mx-1 border rounded bg-white'}>
+                        {currentPage}/{totalPages}
+                    </button>
+
+                    <button onClick={() => handlePageChange(currentPage + 1)} className={'px-4 py-2 mx-1 border rounded bg-white'} disabled={currentPage === totalPages} >
                         Next
                     </button>
                 </div>
-            </div>
+            </div >
         </>
     );
 }
